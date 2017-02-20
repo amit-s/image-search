7b298836c904a7ff3cf27fe7ba7fc4751b6a3cce
amit <shettya@gmail.com>
2017-02-20 09:02:48 -0800

    set up mongodb connectivity

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 'use strict';
 
 let express = require('express');
+let mongo = require('mongodb').MongoClient;
 
 let app = express();
+let mongoURL = 'mongodb://localhost:27017/image_search_api'
 
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'pug');
-app.use('/public/styles', express.static(__dirname + '/public/styles'));
 
-app.get('/', function(req,res){
-	res.render('index');
-});
+mongo.connect(mongoURL, function(err,db){
 
-app.listen(app.get('port'), function(){
-	console.log(`Now listening on port ${app.get('port')}`);
+	if(err)
+		throw new Error(err);
+
+	console.log(`Successfully connected to database`);
+
+	db.collection('images').insert({name: "first entry", type: "test"});
+
+
+
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'pug');
+	app.use('/public/styles', express.static(__dirname + '/public/styles'));
+
+	app.get('/', function(req,res){
+		res.render('index');
+	});
+
+	app.listen(app.get('port'), function(){
+		console.log(`Now listening on port ${app.get('port')}`);
+	});
+
 });
